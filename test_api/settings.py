0e570ca1346e9c1ff8c510d916abb93b959bf84d
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-02-08 17:40:57 +0400

    Change static root settings

--- test_api/settings.py
+++ test_api/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
+STATIC_URL = '/static/rest_framework/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
